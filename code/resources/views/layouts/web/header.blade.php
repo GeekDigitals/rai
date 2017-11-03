909458191c4855dce1a9ff0f7ae1a87bec8e47b0
chandrathen28 <chandrathen28@gmail.com>
2017-11-03 12:04:30 +0700

    first RAI with Github

---
+++ code/resources/views/layouts/web/header.blade.php
@@ -0,0 +1,21 @@
+<!--Styles-->
+    <link href="{{ url('/web/assets/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ url('/web/assets/vendor/bootstrap-4.0.0/css/bootstrap.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('/web/assets/vendor/superslides/css/superslides.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('/web/assets/vendor/owl-carousel/css/owl.carousel.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('/web/assets/vendor/flexslider/css/flexslider.css') }}" rel="stylesheet" type="text/css">
+
+    <!--Fonts-->
+
+    <!--Mechanic Styles-->
+    <link rel="stylesheet" href="{{ url('/web/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('/web/assets/css/fonts.css') }}">
+    <link rel="stylesheet" href="{{ url('/web/assets/css/default.css') }}">
+    <link rel="stylesheet" href="{{ url('/web/assets/css/responsive.css') }}">
+
+
+<!--[if lt IE 9]>
+<link rel="stylesheet" type="text/css" href="{{ url('/') }}/web/vendors/rs-plugin/css/settings-ie8.css" media="screen">
+<script src="{{ url('/') }}/web/js/html5shiv.min.js"></script>
+<script src="{{ url('/') }}/web/js/respond.min.js"></script>
+<![endif]-->
---
+++ code/resources/views/layouts/web/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-toggleable-sm navbar-light bg-faded fixed-top">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+        <img src="{{url('web/images/Logo_RAI_BG Putih.png')}}" class="img-responsive" height="32">
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#products">Products</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
